393f58af21b2308f21c32d9433bae79fb79019c9
Phillip-England <phillip.mark.england@gmail.com>
2025-10-31 06:39:49 -0500

    added ast structures and reasoning about how I am going to take tokens and convert them into ASt nodes

--- internal/wirparser/wirparser.go
+++ internal/wirparser/wirparser.go
@@ -8,16 +8,21 @@ import (
 	"github.com/phillip-england/wir/internal/wirtokenizer"
 )
 
+type AstNode struct {
+	IsRoot bool
+	Children []AstNode
+}
+
+type Ast struct {
+	Root *AstNode
+}
+
 type Parser struct {
 	lexer *runelexer.AbstractLexer[wirtokenizer.Token]
 }
 
 func ParserNew(toks []wirtokenizer.Token) (*Parser, error) {
 	l := runelexer.AbstractLexerNew(toks)
-	l.Iter(func(item wirtokenizer.Token, pos int) bool {
-		fmt.Println(item.Str())
-		return true
-	})
 	err := recursiveParse(l)
 	if err != nil {
 		return &Parser{}, wherr.Consume(wherr.Here(), err, "")
@@ -29,6 +34,7 @@ func ParserNew(toks []wirtokenizer.Token) (*Parser, error) {
 
 func recursiveParse(l *runelexer.AbstractLexer[wirtokenizer.Token]) (error) {
 	l.Iter(func(item wirtokenizer.Token, pos int) bool {
+		fmt.Println(item.Str())
 		return true
 	})	
 	return nil
